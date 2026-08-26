dfbf0a0a003630303c665979f584b1a05af1bd25
りょう <ryo20080827@gmail.com>
2026-08-26 17:31:58 +0900

    PWA対応 (#32)
    * PWA用のHTMLルートとマニフェストファイルを追加
    
    * サーバーサイドレンダリングのためにHTMLルートを更新し、必要な属性を追加

---
+++ app/+html.tsx
@@ -0,0 +1,44 @@
+import {
+  ScrollViewStyleReset,
+  useServerDocumentContext,
+} from "expo-router/html";
+import type { PropsWithChildren } from "react";
+
+// This file is web-only and used to configure the root HTML for every
+// web page during server rendering.
+// The contents of this function only run in Node.js environments and
+// do not have access to the DOM or browser APIs.
+export default function Root({ children }: PropsWithChildren) {
+  const { htmlAttributes, bodyAttributes, headNodes, bodyNodes } =
+    useServerDocumentContext();
+
+  return (
+    <html {...htmlAttributes}>
+      <head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+        <title>RSS Scroll</title>
+
+        {/* Link the PWA manifest file. */}
+        <link rel="manifest" href="/manifest.json" />
+
+        {/*
+          Disable body scrolling on web. This makes ScrollView components work closer to how they do on native.
+          However, body scrolling is often nice to have for mobile web. If you want to enable it, remove this line.
+        */}
+        <ScrollViewStyleReset />
+
+        {/* Add any additional <head> elements that you want globally available on web... */}
+        {headNodes}
+      </head>
+      <body {...bodyAttributes}>
+        {children}
+        {bodyNodes}
+      </body>
+    </html>
+  );
+}
